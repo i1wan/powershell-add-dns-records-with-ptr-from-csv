d1762d35a169a6078d1c8733b893e160594a971f
i1wan <iwan@i-1.nl>
2018-02-20 19:44:28 +0100

    Update add-dns.ps1

--- add-dns.ps1
+++ add-dns.ps1
@@ -6,21 +6,20 @@
 #╚═╝ ╚═╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═══╝
 #By Iwan Hoogendoorn 19 Feb 2018 
 #This script create DNS A Record and associate Reverse PTR Records
-#If the Reverse Lookup Zones do not exist they will be credted as well
-#Create records.csv file with Computer,IP information
+#If the Reverse Lookup Zones do not exist they will be created as well
+#An additional external file is needed (records.csv file) where you create teh records.
 #
 #Change with your dns server info $Servername and $Domain
-$ServerName = "<DNS-SERVER-NAME>"
-$domain = "venus.com"
+$ServerName = "DNS-SERVER-NAME"
+$domain = "domain-name.local"
 Import-Csv c:\script\Records.csv | ForEach-Object {
 
-#Def variable
+#Define variable
 $Computer = "$($_.Computer).$domain"
 $addr = $_.IP -split "\."
 $rzone = "$($addr[2]).$($addr[1]).$($addr[0]).in-addr.arpa"
 
 #Create Dns entries
-
 dnscmd $Servername /recordadd $domain "$($_.Computer)" A "$($_.IP)"
 
 #Create New Reverse Zone if zone already exist, system return a normal error
